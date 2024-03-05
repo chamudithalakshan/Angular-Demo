e86545c6cd3fbf2237741acb0b94d9bfdc559714
chamudithalakshan <chamudithawickramarathna@gmail.com>
2024-03-05 11:39:15 +0530

    routing part done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import {HousingListComponent} from "./housing-list/housing-list.component";
+import {NavbarComponent} from "./navbar/navbar.component";
+import {HomeComponent} from "./home/home.component";
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HousingListComponent, NavbarComponent, RouterLink, RouterLinkActive, HomeComponent,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {ContactComponent} from './contact/contact.component';
+import {MainComponent} from "./main/main.component";
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'contact', component: ContactComponent }
+];
